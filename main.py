025be15f8e89090d4701b198ceb2bca9d8eb03b9
vlh62627 <vlh62627@gmail.com>
2026-02-23 01:43:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-os.environ['GEMINI_API_KEY'] = st.secrets['GEMINI_API_KEY']
+os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 

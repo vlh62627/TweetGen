d366981013ddf39382b2796a395ca7b7d1ce07b0
vlh62627 <vlh62627@gmail.com>
2026-02-23 01:10:33 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import os
+os.environ['GOOGLE_API_KEY'] = "AIzaSyCAdoyMBcPGnvx2FArWMpnELLO56RynkYk"
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+from langchain_core.prompts import PromptTemplate
+tweet_template = "Give me {number} tweets on {topic}"
+tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ["number", "topic"])
+
+gemini3 = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+tweet_Chain = tweet_prompt | gemini3
+
+import streamlit as st
+
+st.header("Tweet Generator")
+
+st.subheader("Generate Tweets using AI")
+
+topic = st.text_input("Topic")
+
+number = st.number_input("No of Tweets", min_value = 1, max_value = 10, value =1, step =1)
+
+if st.button("Generate"):
+	response = tweet_Chain.invoke({"number": number, "topic": topic})
+	st.write(response.content)
